01ba0d2ff52a665f5e2aad1125c7c64ce10b165b
jilanfawwaz <jilanfawwaz@gmail.com>
2022-01-25 13:11:46 +0800

    add some new file and modifying

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -145,4 +145,4 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
-}
+}*/
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage,
+    );
+  }
+}
+
+/*import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+void main() => runApp(const MyApp()); // yang ini beda tapi disamain
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key); // yang ini beda
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      // yang ini beda (const)
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}*/
